bdccc556d2e830eb2c55a54a98434fd45d2a85e5
Michael Nash <michael@spatialnetworks.com>
2019-05-31 13:58:03 -0400

    first pass rna-transcription

--- rna-transcription/rna-transcription.ts
+++ rna-transcription/rna-transcription.ts
@@ -1,6 +1,24 @@
+interface DnaToRna {
+    [nucleotide: string]: string
+}
+
+const dnaToRna: DnaToRna = {
+    G : 'C',
+    C : 'G',
+    T : 'A',
+    A : 'U'
+}
+
 class Transcriptor {
-    toRna( /* Parameters go here */ ) {
-        // Your code here
+    toRna(input: string ) {
+        const nucleotides: string[] = input.split('')
+        return nucleotides.map((nucleotide) => {
+            if (dnaToRna[nucleotide]) {
+                return dnaToRna[nucleotide]
+            } else {
+                throw new Error('Invalid input DNA.')
+            }
+        }).join('')
     }
 }
 
